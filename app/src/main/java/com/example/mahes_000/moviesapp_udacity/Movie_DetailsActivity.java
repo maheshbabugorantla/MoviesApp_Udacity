836e74c76e2a559795fbb11d9e55b45158b5aa43
maheshbabugorantla <mgorantl@purdue.edu>
2016-06-27 11:50:08 -0400

    Made UI Changes to the DetailActivity. Close to Stage 1 Submission. and

--- app/src/main/java/com/example/mahes_000/moviesapp_udacity/Movie_DetailsActivity.java
+++ app/src/main/java/com/example/mahes_000/moviesapp_udacity/Movie_DetailsActivity.java
@@ -43,7 +43,7 @@ public class Movie_DetailsActivity extends AppCompatActivity {
 
     public void markAsFavorite(View view)
     {
-        Toast.makeText(this, "Movie added to the Favorites", Toast.LENGTH_LONG).show();
+        Toast.makeText(this, "Movie added to the Favorites", Toast.LENGTH_SHORT).show();
         return;
     }
 }
